5a7458fbb9baedfc45943242bd3115ceaf53d7ed
Niko2405 <34937693+Niko2405@users.noreply.github.com>
2025-04-10 20:10:52 +0200

    v.0.4
    Redesgin logger output

--- TLogger/Logger.cs
+++ TLogger/Logger.cs
@@ -1,81 +1,97 @@
-﻿using System.Diagnostics;
-
-namespace TLogger
+﻿namespace TLogger
 {
 	public class Logger
 	{
-		public static readonly string VERSION = "0.2";
+		public static readonly string VERSION = "0.4";
 
+		/// <summary>
+		/// Info log
+		/// </summary>
+		/// <param name="text"></param>
 		public static void Info(string text)
 		{
-#pragma warning disable CS8602
-			var methodInfo = new StackTrace().GetFrame(1).GetMethod();
-			var classname = methodInfo.ReflectedType.Name;
-#pragma warning restore CS8602
-
 			Console.BackgroundColor = ConsoleColor.Black;
 			Console.ForegroundColor = ConsoleColor.White;
 			
-			Console.Write($"{GetCurrentTime()} [{classname}]");
+			Console.Write($"{GetCurrentTime()} [");
 
 			Console.ForegroundColor = ConsoleColor.DarkGreen;
-			Console.Write(" INFO ");
+			Console.Write("INFO");
 			Console.ForegroundColor = ConsoleColor.White;
-			Console.WriteLine("   - " + text);
+			Console.WriteLine("]\t|\t" + text);
 
 			// RESET COLOR
 			Console.BackgroundColor = ConsoleColor.Black;
 			Console.ForegroundColor = ConsoleColor.White;
 
-			File.AppendAllTextAsync($"latest.log", $"{GetCurrentTime()} [{classname}] INFO    - {text}\n");
+			File.AppendAllTextAsync($"latest.log", $"{GetCurrentTime()} [INFO]\t|\t{text}\n");
 		}
+
+		/// <summary>
+		/// Warning log
+		/// </summary>
+		/// <param name="text"></param>
 		public static void Warn(string text)
 		{
-#pragma warning disable CS8602
-			var methodInfo = new StackTrace().GetFrame(1).GetMethod();
-			var classname = methodInfo.ReflectedType.Name;
-#pragma warning restore CS8602
-
 			Console.BackgroundColor = ConsoleColor.Black;
 			Console.ForegroundColor = ConsoleColor.White;
 
-			Console.Write($"{GetCurrentTime()} [{classname}]");
+			Console.Write($"{GetCurrentTime()} [");
 
 			Console.ForegroundColor = ConsoleColor.DarkYellow;
-			Console.Write(" WARNING ");
+			Console.Write("WARNING");
 			Console.ForegroundColor = ConsoleColor.White;
-			Console.WriteLine("- " + text);
+			Console.WriteLine("]\t|\t" + text);
 
 			// RESET COLOR
 			Console.BackgroundColor = ConsoleColor.Black;
 			Console.ForegroundColor = ConsoleColor.White;
 
-			File.AppendAllTextAsync($"latest.log", $"{GetCurrentTime()} [{classname}] WARNING - {text}\n");
+			File.AppendAllTextAsync($"latest.log", $"{GetCurrentTime()} [WARNING]\t|\t{text}\n");
 		}
+
+		/// <summary>
+		/// Error log
+		/// </summary>
+		/// <param name="text"></param>
 		public static void Error(string text)
 		{
-#pragma warning disable CS8602
-			var methodInfo = new StackTrace().GetFrame(1).GetMethod();
-			var classname = methodInfo.ReflectedType.Name;
-#pragma warning restore CS8602
-
 			Console.BackgroundColor = ConsoleColor.Black;
 			Console.ForegroundColor = ConsoleColor.White;
 
-			Console.Write($"{GetCurrentTime()} [{classname}]");
+			Console.Write($"{GetCurrentTime()} [");
 
 			Console.ForegroundColor = ConsoleColor.DarkRed;
-			Console.Write(" ERROR ");
+			Console.Write("ERROR");
 			Console.ForegroundColor = ConsoleColor.White;
-			Console.WriteLine("  - " + text);
+			Console.WriteLine("]\t|\t" + text);
 
 			// RESET COLOR
 			Console.BackgroundColor = ConsoleColor.Black;
 			Console.ForegroundColor = ConsoleColor.White;
 
-			File.AppendAllTextAsync($"latest.log", $"{GetCurrentTime()} [{classname}] ERROR   - {text}\n");
+			File.AppendAllTextAsync($"latest.log", $"{GetCurrentTime()} [ERROR]\t|\t{text}\n");
 		}
 
+		/// <summary>
+		/// Delete lastest.log
+		/// </summary>
+		public static void ResetLog()
+		{
+			try
+			{
+				File.Delete("latest.log");
+			}
+			catch (Exception)
+			{
+				return;
+			}
+		}
+
+		/// <summary>
+		/// Get current time
+		/// </summary>
+		/// <returns></returns>
 		private static string GetCurrentTime()
 		{
 			return DateTime.Now.ToString("HH:mm:ss.fff");
