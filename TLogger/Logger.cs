5e0ce287fdfa95ea4568c4b57b11ac95cc37aa6d
Niko2405 <34937693+Niko2405@users.noreply.github.com>
2025-05-04 20:50:20 +0200

    v0.6
    - Add overload functions

--- TLogger/Logger.cs
+++ TLogger/Logger.cs
@@ -2,17 +2,18 @@
 {
 	public class Logger
 	{
-		public static readonly string Version = "0.5";
+		public static readonly string Version = "0.6";
 		
 		/// <summary>
 		/// Enable Debug logging
 		/// </summary>
 		public static bool DebugEnabled = false;
 		
+		#region INFO
 		/// <summary>
 		/// Info log
 		/// </summary>
-		/// <param name="text"></param>
+		/// <param name="text">Message</param>
 		public static void Info(string text)
 		{
 			ResetColorsToDefault();
@@ -28,11 +29,73 @@
 
 			File.AppendAllTextAsync($"latest.log", $"{GetCurrentTime()} [INFO]:\t{text}\n");
 		}
+		
+		/// <summary>
+		/// Info log
+		/// </summary>
+		/// <param name="text">Message</param>
+		public static void Info(int text)
+		{
+			ResetColorsToDefault();
+			
+			Console.Write($"{GetCurrentTime()} [");
 
+			Console.ForegroundColor = ConsoleColor.DarkGreen;
+			Console.Write("INFO");
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine("]:\t" + text);
+
+			ResetColorsToDefault();
+
+			File.AppendAllTextAsync($"latest.log", $"{GetCurrentTime()} [INFO]:\t{text}\n");
+		}
+		
+		/// <summary>
+		/// Info log
+		/// </summary>
+		/// <param name="text">Message</param>
+		public static void Info(float text)
+		{
+			ResetColorsToDefault();
+			
+			Console.Write($"{GetCurrentTime()} [");
+
+			Console.ForegroundColor = ConsoleColor.DarkGreen;
+			Console.Write("INFO");
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine("]:\t" + text);
+
+			ResetColorsToDefault();
+
+			File.AppendAllTextAsync($"latest.log", $"{GetCurrentTime()} [INFO]:\t{text}\n");
+		}
+		
+		/// <summary>
+		/// Info log
+		/// </summary>
+		/// <param name="text">Message</param>
+		public static void Info(bool text)
+		{
+			ResetColorsToDefault();
+			
+			Console.Write($"{GetCurrentTime()} [");
+
+			Console.ForegroundColor = ConsoleColor.DarkGreen;
+			Console.Write("INFO");
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine("]:\t" + text);
+
+			ResetColorsToDefault();
+
+			File.AppendAllTextAsync($"latest.log", $"{GetCurrentTime()} [INFO]:\t{text}\n");
+		}
+		#endregion
+
+		#region WARNING
 		/// <summary>
 		/// Warning log
 		/// </summary>
-		/// <param name="text"></param>
+		/// <param name="text">Message</param>
 		public static void Warn(string text)
 		{
 			ResetColorsToDefault();
@@ -48,11 +111,72 @@
 
 			File.AppendAllTextAsync($"latest.log", $"{GetCurrentTime()} [WARN]:\t{text}\n");
 		}
+		
+		/// <summary>
+		/// Warning log
+		/// </summary>
+		/// <param name="text">Message</param>
+		public static void Warn(int text)
+		{
+			ResetColorsToDefault();
 
+			Console.Write($"{GetCurrentTime()} [");
+
+			Console.ForegroundColor = ConsoleColor.DarkYellow;
+			Console.Write("WARN");
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine("]:\t" + text);
+
+			ResetColorsToDefault();
+
+			File.AppendAllTextAsync($"latest.log", $"{GetCurrentTime()} [WARN]:\t{text}\n");
+		}
+		/// <summary>
+		/// Warning log
+		/// </summary>
+		/// <param name="text">Message</param>
+		public static void Warn(float text)
+		{
+			ResetColorsToDefault();
+
+			Console.Write($"{GetCurrentTime()} [");
+
+			Console.ForegroundColor = ConsoleColor.DarkYellow;
+			Console.Write("WARN");
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine("]:\t" + text);
+
+			ResetColorsToDefault();
+
+			File.AppendAllTextAsync($"latest.log", $"{GetCurrentTime()} [WARN]:\t{text}\n");
+		}
+		
+		/// <summary>
+		/// Warning log
+		/// </summary>
+		/// <param name="text">Message</param>
+		public static void Warn(bool text)
+		{
+			ResetColorsToDefault();
+
+			Console.Write($"{GetCurrentTime()} [");
+
+			Console.ForegroundColor = ConsoleColor.DarkYellow;
+			Console.Write("WARN");
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine("]:\t" + text);
+
+			ResetColorsToDefault();
+
+			File.AppendAllTextAsync($"latest.log", $"{GetCurrentTime()} [WARN]:\t{text}\n");
+		}
+		#endregion
+
+		#region ERROR
 		/// <summary>
 		/// Error log
 		/// </summary>
-		/// <param name="text"></param>
+		/// <param name="text">Message</param>
 		public static void Error(string text)
 		{
 			ResetColorsToDefault();
@@ -70,9 +194,71 @@
 		}
 		
 		/// <summary>
+		/// Error log
+		/// </summary>
+		/// <param name="text">Message</param>
+		public static void Error(int text)
+		{
+			ResetColorsToDefault();
+
+			Console.Write($"{GetCurrentTime()} [");
+
+			Console.ForegroundColor = ConsoleColor.DarkRed;
+			Console.Write("ERROR");
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine("]:\t" + text);
+
+			ResetColorsToDefault();
+
+			File.AppendAllTextAsync($"latest.log", $"{GetCurrentTime()} [ERROR]:\t{text}\n");
+		}
+		
+		/// <summary>
+		/// Error log
+		/// </summary>
+		/// <param name="text">Message</param>
+		public static void Error(float text)
+		{
+			ResetColorsToDefault();
+
+			Console.Write($"{GetCurrentTime()} [");
+
+			Console.ForegroundColor = ConsoleColor.DarkRed;
+			Console.Write("ERROR");
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine("]:\t" + text);
+
+			ResetColorsToDefault();
+
+			File.AppendAllTextAsync($"latest.log", $"{GetCurrentTime()} [ERROR]:\t{text}\n");
+		}
+		
+		/// <summary>
+		/// Error log
+		/// </summary>
+		/// <param name="text">Message</param>
+		public static void Error(bool text)
+		{
+			ResetColorsToDefault();
+
+			Console.Write($"{GetCurrentTime()} [");
+
+			Console.ForegroundColor = ConsoleColor.DarkRed;
+			Console.Write("ERROR");
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine("]:\t" + text);
+
+			ResetColorsToDefault();
+
+			File.AppendAllTextAsync($"latest.log", $"{GetCurrentTime()} [ERROR]:\t{text}\n");
+		}
+		#endregion
+
+		#region DEBUG
+		/// <summary>
 		/// Debug log
 		/// </summary>
-		/// <param name="text"></param>
+		/// <param name="text">Message</param>
 		public static void Debug(string text)
 		{
 			if (!DebugEnabled)
@@ -91,6 +277,77 @@
 
 			File.AppendAllTextAsync($"latest.log", $"{GetCurrentTime()} [DEBUG]:\t{text}\n");
 		}
+		
+		/// <summary>
+		/// Debug log
+		/// </summary>
+		/// <param name="text">Message</param>
+		public static void Debug(int text)
+		{
+			if (!DebugEnabled)
+				return;
+			
+			ResetColorsToDefault();
+
+			Console.Write($"{GetCurrentTime()} [");
+
+			Console.ForegroundColor = ConsoleColor.DarkBlue;
+			Console.Write("DEBUG");
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine("]:\t" + text);
+
+			ResetColorsToDefault();
+
+			File.AppendAllTextAsync($"latest.log", $"{GetCurrentTime()} [DEBUG]:\t{text}\n");
+		}
+		
+		/// <summary>
+		/// Debug log
+		/// </summary>
+		/// <param name="text">Message</param>
+		public static void Debug(float text)
+		{
+			if (!DebugEnabled)
+				return;
+			
+			ResetColorsToDefault();
+
+			Console.Write($"{GetCurrentTime()} [");
+
+			Console.ForegroundColor = ConsoleColor.DarkBlue;
+			Console.Write("DEBUG");
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine("]:\t" + text);
+
+			ResetColorsToDefault();
+
+			File.AppendAllTextAsync($"latest.log", $"{GetCurrentTime()} [DEBUG]:\t{text}\n");
+		}
+		
+		/// <summary>
+		/// Debug log
+		/// </summary>
+		/// <param name="text">Message</param>
+		public static void Debug(bool text)
+		{
+			if (!DebugEnabled)
+				return;
+			
+			ResetColorsToDefault();
+
+			Console.Write($"{GetCurrentTime()} [");
+
+			Console.ForegroundColor = ConsoleColor.DarkBlue;
+			Console.Write("DEBUG");
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine("]:\t" + text);
+
+			ResetColorsToDefault();
+
+			File.AppendAllTextAsync($"latest.log", $"{GetCurrentTime()} [DEBUG]:\t{text}\n");
+		}
+		#endregion
+		
 		
 		/// <summary>
 		/// Print Header with a title
@@ -113,9 +370,9 @@
 			{
 				File.Delete("latest.log");
 			}
-			catch (Exception)
+			catch (Exception e)
 			{
-				return;
+				Error(e.Message);
 			}
 		}
 
